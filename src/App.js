6172293a708e7f40ca4f70c57da81e77a491a618
FotieMConstant <fotiemoghommahieconstant@gmail.com>
2020-10-24 10:54:45 +0100

    Components are all setup

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import Home from './Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1>this is the search Page</h1>
+            
+          </Route>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
